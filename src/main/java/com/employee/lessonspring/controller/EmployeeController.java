75ec174672a765fb2145e8a7cb429e9485397d91
93294 <9329496@mail.ru>
2022-11-13 15:17:24 +0300

    Выполнение домашней работы "Введение в Maven и Spring Boot + web"

--- src/main/java/com/employee/lessonspring/controller/EmployeeController.java
+++ src/main/java/com/employee/lessonspring/controller/EmployeeController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.Set;
 
 /* Методы .maping:
  * GET - получение ресурса
@@ -35,10 +38,23 @@ public class EmployeeController {
         return this.employeeService.addEmployee(employeeRequest);
     }
 
-    @GetMapping("/employee/salary/sum")
+    @GetMapping("/employee/salary/sum") /* Получение суммы зарплат сотрудников */
     int getSalarySum() {
         return this.employeeService.getSalarySum();
     }
+    @GetMapping("/employee/salary/min") /* Получение минимальной зарплаты сотрудника */
+    OptionalInt getSalaryMin() {
+        return this.employeeService.getSalaryMin();
+    }
 
+    @GetMapping("/employee/salary/max") /* Получение максимальной зарплаты сотрудника */
+    OptionalInt getSalaryMax() {
+        return this.employeeService.getSalaryMax();
+    }
+
+    @GetMapping("/employee/salary/aboveaverage")  /* Получение сотрудников с зарплатой выше средней */
+    Set<Employee> getEmployeeSalaryHigherAverage() {
+        return this.employeeService.getEmployeeSalaryHigherAverage();
+    }
 
 }
